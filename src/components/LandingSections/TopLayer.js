7d2ca0c98191a879dcfffaf8d8024560fcfc1786
APPLC-REUBEN <reuben@APPLC-REUBENs-MacBook-Air.local>
2021-04-23 07:57:00 +0100

    how it works section

--- src/components/LandingSections/TopLayer.js
+++ src/components/LandingSections/TopLayer.js
@@ -2,10 +2,12 @@ import React, { Fragment } from "react";
 import { Footer } from "../../components/Footer";
 import '../LandingSections/TopLayer.scss'
 import { GetStarted } from "./GetStarted";
+import HowItWorks from "./HowItWorks";
 
 const TopLayer = () => {
   return (
     <div className='top-layer' >
+      <HowItWorks />
       <GetStarted />
       <Footer />
     </div>
